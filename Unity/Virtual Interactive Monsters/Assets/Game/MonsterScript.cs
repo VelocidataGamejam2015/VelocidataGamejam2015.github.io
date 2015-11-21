ec7074bde3f5f99bcb32118a1bb2bf3f5e998324
Michael Hall <mjhall3@fastmail.net>
2015-11-20 19:59:28 -0600

    Created Unity project for Virtual Interactive Monsters game.

---
+++ Unity/Virtual Interactive Monsters/Assets/Game/MonsterScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterScript : MonoBehaviour {
+
+    public float movement_constant;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x_movement = 0;
+        float y_movement = 0;
+
+	    if (Input.GetKey(KeyCode.UpArrow)) {
+            y_movement += movement_constant;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            y_movement -= movement_constant;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            x_movement -= movement_constant;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            x_movement += movement_constant;
+        }
+        transform.Translate(x_movement, y_movement, 0);
+    }
+}
